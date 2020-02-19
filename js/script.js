48b345fb16e11a9bb16b6538c87b75772d10a478
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-02-19 16:10:46 +0300

    added js for form vaidation

--- js/script.js
+++ js/script.js
@@ -20,4 +20,32 @@ $(document).ready(function() {
     });
 });
 
+var btn = document.getElementById("myBtn");
+
+// When the user clicks on the button
+function validate() {
+    // Validate form for entry
+    var x = document.forms["myForm"]["contact-name"].value;
+    if (x == "") {
+        alert("Name not entered");
+        return false;
+    }
+    var y = document.forms["myForm"]["contact-email"].value;
+    if (y == "") {
+        alert("Email not entered");
+        return false;
+    }
+    var z = document.forms["myForm"]["contact-message"].value;
+    if (z == "") {
+        alert("You have no message to send");
+        return false;
+    }
+    var person1Input = $("input#contact-name").val();
+    $("span#outputName").append(person1Input);
+
+    $('.toast').toast('show');
+    event.preventDefault();
+}
+
+
 
